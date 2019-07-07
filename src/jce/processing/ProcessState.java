fc97a6d24bc58c1289b88cbb335079c1a87bb124
Mehdi <mehditeymorian322@gmail.com>
2019-07-07 20:53:02 +0430

    Change Access-Modifier to Package

--- src/jce/processing/ProcessState.java
+++ src/jce/processing/ProcessState.java
@@ -1,6 +1,6 @@
 package jce.processing;
 
-public class ProcessState {
+ class ProcessState {
     static final int RUNNING = 0;
     static final int TIME_EXCEEDED = 1;
     static final int FINISHED_BEFORE_EXCEED = 2;

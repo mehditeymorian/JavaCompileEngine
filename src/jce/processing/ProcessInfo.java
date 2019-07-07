e21303d4a6c7aa1fd88875bac511381c17678f8b
Mehdi <mehditeymorian322@gmail.com>
2019-07-07 20:49:24 +0430

    Update Process Element to Map<Integer, Process>

--- src/jce/processing/ProcessInfo.java
+++ src/jce/processing/ProcessInfo.java
@@ -4,9 +4,11 @@ package jce.processing;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Map;
+
 @Builder
 class ProcessInfo {
-    @Getter private Process process;
+    @Getter private Map<Integer,Process> process;
     @Getter private String log;
     @Getter private String[] commands;
 }

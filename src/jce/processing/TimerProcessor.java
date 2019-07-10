1c81118a93e68b293b469086fc16cb06f14e99ba
Mehdi <mehditeymorian322@gmail.com>
2019-07-10 11:22:04 +0430

    BasicProcessor with TimeExceedChecker. if processor exceeded the given time, it will be stopped

---
+++ src/jce/processing/TimerProcessor.java
@@ -0,0 +1,61 @@
+package jce.processing;
+
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import static jce.processing.TimerProcessorState.*;
+
+@RequiredArgsConstructor
+public class TimerProcessor implements OnFinishListener {
+
+    private Thread timer;
+    private BasicProcessor processor;
+    private OnFinishListener onFinishListener;
+
+    @NonNull private String[] commands;
+    @NonNull private int timeExceedInMillis;
+
+    private TimerProcessorState processorState = NOT_FINISHED; // default value -- none of the options
+
+    public void start(){
+        onFinishListener = this;
+        timer = getTimer();
+        processor = getProcessor();
+
+        timer.start();
+        processor.start();
+    }
+
+    @Override
+    public void OnFinish(TimerProcessorState state) {
+        processorState = state;
+        switch (state){
+            case TIME_EXCEEDED:// Time Exceeded
+                processor.interrupt();
+                break;
+
+            case TASK_FINISHED_EARLY:// Process Finished Before Time Exceeded
+                timer.interrupt();
+                break;
+        }
+    }
+
+    private Thread getTimer(){
+        return new Thread(() -> {
+            // Timer Runnable
+            try { Thread.sleep(timeExceedInMillis); }
+            catch (InterruptedException ignored) {/* No Action Needed */ }
+            if (processorState.taskNotFinishedEarly()) onFinishListener.OnFinish(TIME_EXCEEDED);
+
+        });
+    }
+
+    private BasicProcessor getProcessor(){
+        return new BasicProcessor(commands, () -> {
+            // After Process
+            if (processorState.timeNotExceeded()) onFinishListener.OnFinish(TASK_FINISHED_EARLY);
+        });
+    }
+
+}
